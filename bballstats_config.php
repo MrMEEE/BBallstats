e392b5889e4dcf71a61a96aa4847ddaeb7e05d4e
MrMEEE <mj@casalogic.dk>
2012-08-14 22:44:39 +0200

    spiller oprettelse menu + Masser af rettelser + nye ikoner..

--- bballstats_config.php
+++ bballstats_config.php
@@ -19,8 +19,9 @@ getThemeTitle("Statistik Konfiguration");
 require("../../menu.php");
 
 if(!mysql_num_rows(mysql_query("SHOW TABLES LIKE 'bballstats_config'"))){
-      mysql_query("CREATE TABLE `bballstats_config` (`id` int(11) NOT NULL AUTO_INCREMENT, `hold` text NOT NULL,PRIMARY KEY (`id`)) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;");
-      mysql_query("INSERT INTO `bballstats_config` SET `id`=1");
+      mysql_query("CREATE TABLE `bballstats_config` (`id` int(11) NOT NULL AUTO_INCREMENT, `hold` text NOT NULL,PRIMARY KEY (`id`)) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1");
+      mysql_query("INSERT INTO `bballstats_config` SET `id`=1,`hold`=''");
+      mysql_query("CREATE TABLE `bballstats_players` (`id` int(11) NOT NULL AUTO_INCREMENT, `hold` int(11) NOT NULL , `fornavn` text NOT NULL,`efternavn` text NOT NULL, `beskrivelse` text NOT NULL, `nummer` text NOT NULL, `position` text NOT NULL,  PRIMARY KEY (`id`)) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1");
 }
 
 $config = mysql_fetch_assoc(mysql_query("SELECT * FROM `bballstats_config` WHERE `id`=1"));
@@ -42,6 +43,9 @@ if(isset($_GET['remove'])){
       if(in_array($_GET['remove'],$hold)){
             $hold = array_diff($hold,array($_GET['remove']));
             $holdstr = implode(",",$hold);
+            if($holdstr == ","){
+                  $holdstr = "";
+            }
             mysql_query("UPDATE `bballstats_config` SET `hold`='".$holdstr."' WHERE `id`='1'");
       }
 }
--- bballstats_pluginmenu.php
+++ bballstats_pluginmenu.php
@@ -4,6 +4,7 @@ echo '
 <li class="dir">Statistik
         <ul>';
                   if (checkAdmin($_SESSION['username'])){
+                          echo '<li class="first"><a href="http://' . $klubadresse . $klubpath . '/admin/plugins/bballstats/bballstats_players.php">Spillere</a></li>';
                           echo '<li class="last"><a href="http://' . $klubadresse . $klubpath . '/admin/plugins/bballstats/bballstats_config.php">Konfiguration</a></li>';
                   }
                   echo '
